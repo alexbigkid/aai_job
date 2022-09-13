bffef20683b67c642e3cbda3a370f2a9cfce7195
Alex Berger <alexbigkid@users.noreply.github.com>
2022-09-13 16:52:32 -0700

    R terraform added privacy to the S3 bucket for saving the terraform state files

--- terraform/templates/terraformStateBootstrap/main.tf
+++ terraform/templates/terraformStateBootstrap/main.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "aaiTerraformStateS3Bucket" {
     bucket = "aai-terraform-state-do-not-delete-${var.env}"
+}
+
+resource "aws_s3_bucket_acl" "aaiTerraformStateS3Bucket" {
+    bucket = aws_s3_bucket.aaiTerraformStateS3Bucket.id
     acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "aaiTerraformStateS3Bucket" {
+    bucket = aws_s3_bucket.aaiTerraformStateS3Bucket.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
 }
 
 resource "aws_dynamodb_table" "aaiDynamodbTerraformLock" {
